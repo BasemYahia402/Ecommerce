669cb9858135d94a9e455062c8b983d1070c8273
BasemYahia402 <basemyahia70@gmail.com>
2024-06-30 15:47:24 +0300

    Update path of base

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/Ecommerce/",
+  base: "/",
   build: {
     outDir: "dist",
   },
