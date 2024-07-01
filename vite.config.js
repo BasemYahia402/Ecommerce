0c671a2dfdf9ebcd55a60dec551bf97e574a4205
BasemYahia402 <basemyahia70@gmail.com>
2024-07-01 11:45:43 +0300

    Remove base and build

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/Ecommerce/",
-  build: {
-    outDir: "dist",
-  },
   plugins: [react()],
 });
